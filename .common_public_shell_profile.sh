fb9ecae160d8012cd04761af7281b0395d8828c7
Steve <stephendwebster@gmail.com>
2016-07-17 20:42:41 -0400

    Begin separating bash prof, committing  generally applicable fns

---
+++ .common_public_shell_profile.sh
@@ -0,0 +1,30 @@
+
+# Meta aliases
+alias prof="vim ~/.bash_profile"
+alias reprof=". ~/.bash_profile"
+alias vir="vim ~/.vimrc"
+alias tprof="vim ~/.tmux.conf"
+alias subl="open -a Sublime\ Text\ 2"
+
+## Clear terminal window (without actually deleting the old buffer)
+alias rebuff="clear && printf '\e[3J'"
+
+# Navigational aliases
+alias de="cd ~/Desktop"
+alias ds="cd ~/Documents"
+
+# Files
+mkcd () {
+  mkdir "$1"
+  cd "$1"
+}
+
+cdl () {
+  cd "$1"
+  ls
+}
+
+# dvorak home row synonym for ls: [a]ll files, [e]very one
+alias ae="ls -a"
+# [a]ll files visible to common [u]ser (e.g. in Finder)
+alias au="ls"
