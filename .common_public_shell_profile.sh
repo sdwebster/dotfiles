1f8725bb83e0dec35158c4f3a3accc57e055c445
Steve <stephendwebster@gmail.com>
2016-08-19 16:52:27 -0400

    Add more useful aliases

--- .common_public_shell_profile.sh
+++ .common_public_shell_profile.sh
@@ -9,9 +9,25 @@ alias subl="open -a Sublime\ Text\ 2"
 ## Clear terminal window (without actually deleting the old buffer)
 alias rebuff="clear && printf '\e[3J'"
 
-# Navigational aliases
+## Navigational aliases for general OS X
 alias de="cd ~/Desktop"
 alias ds="cd ~/Documents"
+alias dwl="cd ~/Downloads"
+
+# View & grab the latest from your Downloads & Desktop
+alias lsd="ls -tr $HOME/Downloads" # sorted most- to least-recently modified
+alias cldl="cp \"$HOME/Downloads/$(lsd | tail -1)\"" # copy last download, e.g. $ cldl .
+alias mldl="mv \"$HOME/Downloads/$(lsd | tail -1)\"" # move last download, e.g. $ mldl .
+
+alias lsdk="ls -tr $HOME/Desktop" # sorted most- to least-recently modified
+alias cldk="cp \"$HOME/Desktop/$(lsdk | tail -1)\""
+alias mldk="mv \"$HOME/Desktop/$(lsdk | tail -1)\""
+
+## Find the dimensions of an image (check out "$ man sips" for more awesome)
+alias pic-dimensions="sips -g pixelHeight -g pixelWidth" # e.g. $ pic-dimensions site-logo.png
+
+## Lock screen w/o logout
+alias lockscreen='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
 
 # Files
 mkcd () {
